0227b787cca4ce678f87e279183cb65fc2a32851
maowc <maowc@chyjr.com>
2017-01-19 10:03:06 +0800

    TestContinue的新增 test3

--- src/com/test/continue1/TestContinue.java
+++ src/com/test/continue1/TestContinue.java
@@ -21,4 +21,15 @@ public class TestContinue {
 			System.out.println(i);
 		}
 	}
+	@Test
+	public void test3(){
+		for(int i=0;i<5;i++){
+			for(int j=0;j<5;j++){
+				if(j==2){
+					break;
+				}
+				System.out.println(i +"*"+j+"="+ i*j);
+			}
+		}
+	}
 }
