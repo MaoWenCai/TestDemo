5c19e6148c66d985da6bb558b2adc356924ca855
maowc <maowc@chyjr.com>
2017-01-19 10:07:02 +0800

    TestContinue新增test5方法-return

--- src/com/test/continue1/TestContinue.java
+++ src/com/test/continue1/TestContinue.java
@@ -44,4 +44,16 @@ public class TestContinue {
 			}
 		}
 	}
+	
+	@Test
+	public void test5(){
+		for(int i=0;i<5;i++){
+			for(int j=0;j<5;j++){
+				if(j==2){
+					return;
+				}
+				System.out.println(i +"*"+j+"="+ i*j);
+			}
+		}
+	}
 }
