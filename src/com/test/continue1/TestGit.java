d2b400efc023c5d0b7711a581c625ebe8ae56161
maowc <maowc@chyjr.com>
2017-01-19 13:45:55 +0800

    TestGit 新增方法 test

--- src/com/test/continue1/TestGit.java
+++ src/com/test/continue1/TestGit.java
@@ -1,7 +1,14 @@
 package com.test.continue1;
 
+import org.junit.Test;
+
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("本地1");
+	}
+	
+	@Test
+	public void test(){
+		System.out.println("aaaaa");
 	}
 }
