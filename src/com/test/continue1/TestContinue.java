75039d37e6798247097ad7e40c65ed403ec532dc
maowc <maowc@chyjr.com>
2017-01-19 10:37:17 +0800

    删除新增的方法345

--- src/com/test/continue1/TestContinue.java
+++ src/com/test/continue1/TestContinue.java
@@ -21,44 +21,4 @@ public class TestContinue {
 			System.out.println(i);
 		}
 	}
-	@Test
-	public void test3(){
-		for(int i=0;i<5;i++){
-			for(int j=0;j<5;j++){
-				if(j==2){
-					break;
-				}
-				System.out.println(i +"*"+j+"="+ i*j);
-			}
-		}
-	}
-	
-	@Test
-	public void test4(){
-		for(int i=0;i<5;i++){
-			for(int j=0;j<5;j++){
-				if(j==2){
-					continue;
-				}
-				System.out.println(i +"*"+j+"="+ i*j);
-			}
-		}
-	}
-	
-	@Test
-	public void test5(){
-		for(int i=0;i<5;i++){
-			for(int j=0;j<5;j++){
-				if(j==2){
-					return;
-				}
-				System.out.println(i +"*"+j+"="+ i*j);
-			}
-		}
-	}
-	/**return 测试
-	 * 返回值
-	 * 0*0=0
-	   0*1=0
-	 */
 }
