999323a4e5ede03f9e0d7632abb362ab48ae5f3f
maowc <maowc@chyjr.com>
2017-01-19 13:29:56 +0800

    Revert "TestGit  输出"
    This reverts commit a3e28561b2b2cc205e5799d0d6124b5d0d22180e.
    
    # Conflicts:
    #	build/classes/com/test/continue1/TestGit.class
    #	src/com/test/continue1/TestGit.java

--- src/com/test/continue1/TestGit.java
+++ src/com/test/continue1/TestGit.java
@@ -3,7 +3,5 @@ package com.test.continue1;
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("本地1");
-		System.out.println("本地2");
-		System.out.println("本地3");
 	}
 }
