63f14f0aa97d28969d67349bc06ddd26f6381420
maowc <maowc@chyjr.com>
2017-01-19 10:05:27 +0800

    TestContinue添加test4方法-continue

--- src/com/test/continue1/TestContinue.java
+++ src/com/test/continue1/TestContinue.java
@@ -32,4 +32,16 @@ public class TestContinue {
 			}
 		}
 	}
+	
+	@Test
+	public void test4(){
+		for(int i=0;i<5;i++){
+			for(int j=0;j<5;j++){
+				if(j==2){
+					continue;
+				}
+				System.out.println(i +"*"+j+"="+ i*j);
+			}
+		}
+	}
 }
